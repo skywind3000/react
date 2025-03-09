691183475142df7d55af2888e0a434656634cbcc
skywind3000 <skywind3000@163.com>
2025-03-10 02:52:33 +0800

    update files

--- tutor/route-demo1/src/pages/Contact.js
+++ tutor/route-demo1/src/pages/Contact.js
@@ -1,6 +1,15 @@
+import React from 'react';
+import {useRef, useState, useEffect} from 'react';
+
 const Contact = () => {
-    return <h1>Contact Me</h1>;
+    const previous = useRef(10);
+    const [count, setCount] = useState(0);
+    previous.current += 1;
+    return (<>
+    <h1>Contact Me {previous.current} !</h1>;
+    <h2>Count: {count} !!</h2>
+    <button onClick={() => {setCount(count+1);}}>Contact Me</button>
+    </>);
 };
-  
+
 export default Contact;
-  
