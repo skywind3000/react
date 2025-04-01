edc169381269483fde16e3d23cabbdbb322d4564
skywind3000 <skywind3000@163.com>
2025-04-01 22:44:10 +0800

    update main.js

--- electron/electron1/src/App.js
+++ electron/electron1/src/App.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { ipcRenderer } from 'electron';
 
 export default function App() {
-  return <h1>Hello, World!!</h1>;
+	const [count, setCount] = useState(0);
+	const [text, setText] = useState('');
+	useEffect(() => {
+		const handleAppPath = async () => {
+			const appPath = await ipcRenderer.invoke('get-app-path');
+			setText(appPath);
+		};
+		handleAppPath();
+	}, [])
+	async function onClick() {
+		setCount(count + 1);
+		const appPath = await ipcRenderer.invoke('get-os-platform');
+		setText(appPath);
+	}
+	
+	return (<>
+		<h1>Hello, World!!</h1>
+		<p>Welcome to my first React app.</p>
+		<p>Count: {count}</p>
+		<p>Text: {text}</p>
+		<button onClick={onClick}>Increment</button>
+	</>);
 }
